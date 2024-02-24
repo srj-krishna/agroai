d135b155c4542385445a02a43e3ce6037b04422c
srj-krishna <108737525+srj-krishna@users.noreply.github.com>
2024-02-24 06:52:41 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -42,7 +42,7 @@ st.set_page_config(
 version = embedchain.__version__
 st.title("💬 AgroGPT")
 st.caption("🚀 developed by NeuBiom Labs!")
-system_message = "You are an Agribot, here to help with information and context-specific recommendations for farming in Kerala for the following query. If you don't know something just politely say that you don't have the information."
+system_message = "You are an Agribot, here to help with information and context-specific recommendations for farming in Kerala for the following query. If you don't know something just say that you don't have the information."
 lang = "English"
 final_prompt = ""
 
