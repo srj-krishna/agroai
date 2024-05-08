8df5732607e6b5be9b93190d37697403f727b6b6
srj-krishna <108737525+srj-krishna@users.noreply.github.com>
2024-05-08 13:22:33 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -7,7 +7,9 @@ from azure.ai.translation.text import TextTranslationClient, TranslatorCredentia
 from azure.ai.translation.text.models import InputTextItem
 from azure.core.exceptions import HttpResponseError
 from streamlit_geolocation import streamlit_geolocation
+import requests
 
+WEATHER_KEY = "a83787e98421eae60ced116f70771a85"
 os.environ["HUGGINGFACE_ACCESS_TOKEN"] = "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"
 os.environ["PINECONE_API_KEY"] = "9a3d0633-db06-4ef7-a49e-3fae7210b765"
 
