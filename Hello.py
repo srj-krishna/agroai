03b37c11acc74e06eef07d17b5f8524daa601fb2
srj-krishna <108737525+srj-krishna@users.noreply.github.com>
2024-05-08 12:41:52 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -84,7 +84,7 @@ with st.sidebar:
 
     
     # Display a message while waiting for geolocation
-    st.write("Share location")
+    st.write("Share my location")
     # Display the JavaScript to get geolocation
     loc = streamlit_geolocation()
     st.write(f"Your coordinates are {loc}")
