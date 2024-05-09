8cedbb1cfa9a7db9f87f38d8e4427441f2d3e20f
srj-krishna <108737525+srj-krishna@users.noreply.github.com>
2024-05-09 05:43:29 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -166,7 +166,7 @@ with st.sidebar:
             
 st.caption("💬 Language set to " + lang)
 querylog = chathistory_dataframe()
-conn.update(worksheet="userlog", data=querylog)
+#conn.update(worksheet="userlog", data=querylog)
 if prompt := st.chat_input("Ask me anything!"):
     app = agroneugraph()
  
