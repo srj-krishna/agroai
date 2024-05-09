cc7196f765a86831a97806252038d77ef9f8faf1
srj-krishna <108737525+srj-krishna@users.noreply.github.com>
2024-05-09 04:33:59 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -167,7 +167,7 @@ with st.sidebar:
             
 st.caption("💬 Language set to " + lang)
 querylog = chathistory_dataframe()
-#conn.create(spreadsheet=dbsheeturl, worksheet="AgroNeuBot_log", data=querylog)
+conn.create(spreadsheet=dbsheeturl, worksheet="AgroNeuBot_log", data=querylog)
 if prompt := st.chat_input("Ask me anything!"):
     app = agroneugraph()
  
